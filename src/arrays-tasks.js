80ce1860409dd4ce8f6b6107f54036a855ee05b5
Alena1409 <a77993519@gmail.com>
2024-11-28 11:06:46 +0400

    feat: add function sortDigitNamesByNumericOrder

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -569,8 +569,24 @@ function shiftArray(/* arr, n */) {
  *   sortDigitNamesByNumericOrder([ 'nine','eight','nine','eight' ]) => [ 'eight','eight','nine','nine']
  *   sortDigitNamesByNumericOrder([ 'one','one','one','zero' ]) => [ 'zero','one','one','one' ]
  */
-function sortDigitNamesByNumericOrder(/* arr */) {
-  throw new Error('Not implemented');
+function sortDigitNamesByNumericOrder(arr) {
+  const numbers = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+  ];
+  if (arr.length === 0) return [];
+  return arr
+    .map((item) => numbers.indexOf(item))
+    .sort()
+    .map((item) => numbers[item]);
 }
 
 /**
